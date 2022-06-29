9dedfa33f2f6ba2e50b46476c683122f037e7a0d
Soham Sarkar <soham.ss1@gmail.com>
2022-06-29 17:39:11 +0530

    update and fix prisma migrations and setup

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,7 @@ class App {
 
   public listen() {
     this.app.listen(this.port, () => {
+      logger.info(process.env.DATABASE_URL)
       logger.info(`=================================`);
       logger.info(`======= ENV: ${this.env} =======`);
       logger.info(`🚀 App listening on the port ${this.port}`);
